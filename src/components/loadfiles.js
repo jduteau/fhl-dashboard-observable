c82443c9d1bf497a6d3a964b393277e7587126c9
Jean Duteau <jean@duteaudesign.com>
2026-01-05 17:21:09 -0700

    Period 14 roster changes

--- src/components/loadfiles.js
+++ src/components/loadfiles.js
@@ -41,6 +41,7 @@ export const statsPeriods = [
   { period: 11, data: await readStatsFile("src/data/stats_p11.csv") },
   { period: 12, data: await readStatsFile("src/data/stats_p12.csv") },
   { period: 13, data: await readStatsFile("src/data/stats_p13.csv") },
+  { period: 14, data: await readStatsFile("src/data/stats_p14.csv") },
   // Add more periods here as files become available:
   // { period: 4, data: await readStatsFile("src/data/stats_p04.csv") },
   // etc...
@@ -63,6 +64,7 @@ export const rosterPeriods = [
   { period: 11, data: await csvParse(stripBom(readFileSync("src/data/rosters_p11.csv", "utf-8"))) },
   { period: 12, data: await csvParse(stripBom(readFileSync("src/data/rosters_p12.csv", "utf-8"))) },
   { period: 13, data: await csvParse(stripBom(readFileSync("src/data/rosters_p13.csv", "utf-8"))) },
+  { period: 14, data: await csvParse(stripBom(readFileSync("src/data/rosters_p14.csv", "utf-8"))) },
   // Add more periods here as files become available:
   // { period: 4, data: await csvParse(stripBom(readFileSync("src/data/rosters_p04.csv", "utf-8"))) },
   // etc...
