654cc7fb9198c49573241376017a631e15df50f9
Jean Duteau <jean@duteaudesign.com>
2026-04-07 09:33:19 -0600

    Period 24 rosters and Apr 6 stats

--- src/components/loadfiles.js
+++ src/components/loadfiles.js
@@ -51,7 +51,7 @@ export const statsPeriods = [
   { period: 21, data: await readStatsFile("src/data/static/stats/stats_p21.csv") },
   { period: 22, data: await readStatsFile("src/data/static/stats/stats_p22.csv") },
   { period: 23, data: await readStatsFile("src/data/static/stats/stats_p23.csv") },
-//  { period: 24, data: await readStatsFile("src/data/static/stats/stats_p24.csv") },
+  { period: 24, data: await readStatsFile("src/data/static/stats/stats_p24.csv") },
 //  { period: 25, data: await readStatsFile("src/data/static/stats/stats_p25.csv") },
 ];
 
@@ -80,7 +80,7 @@ export const rosterPeriods = [
   { period: 21, data: await csvParse(stripBom(readFileSync("src/data/static/rosters/rosters_p21.csv", "utf-8"))) },
   { period: 22, data: await csvParse(stripBom(readFileSync("src/data/static/rosters/rosters_p22.csv", "utf-8"))) },
   { period: 23, data: await csvParse(stripBom(readFileSync("src/data/static/rosters/rosters_p23.csv", "utf-8"))) },
-//  { period: 24, data: await csvParse(stripBom(readFileSync("src/data/static/rosters/rosters_p24.csv", "utf-8"))) },
+  { period: 24, data: await csvParse(stripBom(readFileSync("src/data/static/rosters/rosters_p24.csv", "utf-8"))) },
 //  { period: 25, data: await csvParse(stripBom(readFileSync("src/data/static/rosters/rosters_p25.csv", "utf-8"))) },
 ];
 
